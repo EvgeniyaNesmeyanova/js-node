9ac7f4187900ac728038c05ea51009bc1c5a4ff3
EvgeniyaNesmeyanova <prof_nea@mail.ru>
2024-08-19 12:14:22 +0500

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,29 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
   {languageOptions: { globals: globals.browser }},
+  {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error"
+    }
+}
+,
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
